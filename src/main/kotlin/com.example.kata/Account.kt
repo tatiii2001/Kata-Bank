1bf6d89d60f265dda3e353fcfe251d51bde28b1b
tatiii2001 <tatiiigomez2001@gmail.com>
2024-04-11 08:47:26 +0100

    Refactor: collect in a variable the current day

--- src/main/kotlin/com.example.kata/Account.kt
+++ src/main/kotlin/com.example.kata/Account.kt
@@ -7,18 +7,20 @@ class Account : AccountService{
 
     data class Action(val dataTime : String, val amount: String, val balance : Int)
 
-    var actions = mutableListOf<Action>()
+    private var actions = listOf<Action>()
     var balance = 0
-    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+
+    private val currentDate = LocalDateTime.now().format(formatter)
 
     override fun deposit(amount: Int) {
         this.balance += amount
-        this.actions.add(Action(LocalDateTime.now().format(formatter), amount.toString(), this.balance))
+        this.actions.toMutableList().add(Action(currentDate, amount.toString(), this.balance))
     }
 
     override fun withdraw(amount: Int) {
         this.balance -= amount
-        this.actions.add(Action(LocalDateTime.now().format(formatter), "-$amount", this.balance))
+        this.actions.toMutableList().add(Action(currentDate, "-$amount", this.balance))
     }
 
     override fun printStatement() {
@@ -26,7 +28,7 @@ class Account : AccountService{
             "Date         ||  Amount  ||  Balance"
         )
         actions.forEach {
-            println(it.dataTime+"   ||"+it.amount+"      ||    "+it.balance)
+            println(it.dataTime+"   ||  "+it.amount+"     ||    "+it.balance)
         }
     }
 
--- src/test/kotlin/com.example.kata/AccountTest.kt
+++ src/test/kotlin/com.example.kata/AccountTest.kt
@@ -4,26 +4,24 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class AccountTest {
-
-
     @Test
     fun deposit() {
         val account = Account()
 
         account.deposit(500)
 
+        account.printStatement()
+
         assertEquals(500, account.balance)
     }
-
-
     @Test
     fun withdraw(){
         val account = Account()
 
         account.withdraw(500)
+
         account.printStatement()
+
         assertEquals(-500, account.balance)
     }
-
-
 }
